f0fcf825a3faa26e52dabe6d88abd35acc9d97e9
byube1 <tieurua1@gmail.com>
2021-04-09 22:01:57 +0700

    init

---
+++ WebAPIMongoDB/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIMongoDB.Models;
+using WebAPIMongoDB.Services;
+
+namespace WebAPIMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> Get() =>
+            _productService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public ActionResult<Product> Get(string id)
+        {
+            var product = _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            _productService.Create(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Product productIn)
+        {
+            var book = _productService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _productService.Update(id, productIn);
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebAPIMongoDB/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using WebAPIMongoDB.Models;
+
+namespace WebAPIMongoDB.Services
+{
+    
+    public class ProductService
+    {
+        private readonly IMongoCollection<Product> _product;
+
+        public ProductService(IProductDbDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _product = database.GetCollection<Product>(settings.ProductCollectionName);
+        }
+
+        public List<Product> Get() =>
+            _product.Find(pd => true).ToList();
+
+        public Product Get(string id) =>
+            _product.Find<Product>(book => book.Id == id).FirstOrDefault();
+
+        public Product Create(Product pd)
+        {
+            _product.InsertOne(pd);
+            return pd;
+        }
+
+        public void Update(string id, Product pdIn) =>
+            _product.ReplaceOne(pd => pd.Id == id, pdIn);
+
+        public void Remove(Product pdIn) =>
+            _product.DeleteOne(pd => pd.Id == pdIn.Id);
+
+        public void Remove(string id) =>
+            _product.DeleteOne(pd => pd.Id == id);
+    }
+}
