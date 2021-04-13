66ed8192a2f2fac0b12c11137358026329f81b84
byube1 <tieurua1@gmail.com>
2021-04-13 15:39:19 +0700

    init

---
+++ WebAPIMongoDB/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebAPIMongoDB.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+        [BsonElement("catelogy")]
+        public string Category { get; set; }
+        [BsonElement("dateCreate")]
+        public DateTime DateCreate { get; set; }
+        [BsonElement("img")]
+        public string Img { get; set; }
+
+    }
+}
---
+++ WebAPIMongoDB/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using WebAPIMongoDB.Models;
+using WebAPIMongoDB.Services;
+
+namespace WebAPIMongoDB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<ProductDbDatabaseSetting>(
+                Configuration.GetSection(nameof(ProductDbDatabaseSetting)));
+
+            services.AddSingleton<IProductDbDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<ProductDbDatabaseSetting>>().Value);
+
+            services.AddSingleton<ProductService>();
+            services.AddCors();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
