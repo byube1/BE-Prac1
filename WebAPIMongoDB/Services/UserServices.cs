c5ad48f12daa53c4498575207f1fa61692924f71
byube1 <tieurua1@gmail.com>
2021-04-16 10:38:54 +0700

    feat: update jwt

---
+++ WebAPIMongoDB/Services/UserServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using WebAPIMongoDB.Models;
+
+namespace WebAPIMongoDB.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress);
+        AuthenticateResponse RefreshToken(string token, string ipAddress);
+        bool RevokeToken(string token, string ipAddress);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserServices 
+
+    {
+        private readonly IMongoCollection<User> _user;
+
+        public UserServices(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _user = database.GetCollection<User>("customer");
+        }
+    }
+}
